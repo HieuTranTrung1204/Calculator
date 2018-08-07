79144d9e7470d5358a53b0212ff0dc74620facae
Trần Trung Hiếu <hieu.trantrung1204@icloud.com>
2018-08-07 17:14:27 +0700

    + tính năng: ToastNotification

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# Universal Windows Platform
-![http://cdn1.theouterhaven.net/wp-content/uploads/2016/05/UWP-Small-Windows-10.jpg](http://cdn1.theouterhaven.net/wp-content/uploads/2016/05/UWP-Small-Windows-10.jpg)
-## 1. Calculator
+# Part 1: UWP basic
+
+## *Style
+- https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/xaml-styles 
+- https://docs.microsoft.com/en-us/windows/uwp/design/basics/xaml-basics-style 
+
+## * Toast notificaiton
+add lib
+  ```
+    Microsoft.Toolkit.Uwp.Notifications
+    QueryString.NET
+  ```
+using lib:
+  ```
+    using Windows.UI.Notifications;
+    using Microsoft.Toolkit.Uwp.Notifications; // Notifications library
+    using Microsoft.QueryStringDotNET; // QueryString.NET
+  ```
+
+## * Tiles
+- https://docs.microsoft.com/en-us/windows/uwp/design/shell/tiles-and-notifications/creating-tiles 
+
+
+## * GameDemo
+- https://code.msdn.microsoft.com/windowsapps/Tic-Tac-Toe-in-Windows-10-d50a7a27/sourcecode?fileId=146059&pathId=660827241
+- https://corina.microsoft.pub.ro/2016/08/27/how-to-create-a-simple-game-using-universal-windows-platform/  
+
+-----------------------------------------------
+# Part 2: Game với UWP 
+## *WCP
+- https://confluence.gameloft.org/display/W8KB/Workshop+WCPv2
+- 
--- UWPFeatures/UWPFeatures/MainPage.xaml.cs
+++ UWPFeatures/UWPFeatures/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Xml.Dom;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +29,141 @@ namespace UWPFeatures
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        void ShowToastNotificationScheduled(int timeDelay)
+        {
+            string xml = @"<toast>
+                                <visual>
+                                <binding template=""ToastGeneric"">
+                                    <text>Hello!</text>
+                                    <text>This is a scheduled toast!</text>
+                                </binding>
+                                </visual>
+                            </toast>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            ScheduledToastNotification toast = new ScheduledToastNotification(doc, DateTimeOffset.Now.AddSeconds(timeDelay));
+            ToastNotificationManager.CreateToastNotifier().AddToSchedule(toast);
+        }
+        void ShowToastNotification()
+        {
+            // In a real app, these would be initialized with actual data
+            string title = "Title notificaiton sent you a picture";
+            string content = "Check this out, Happy Coding!";
+            string image = "http://p6i771owklq1zv0eh1wqu4n1-wpengine.netdna-ssl.com/wp-content/uploads/2016/01/Gameloft-Gets-With-AppNexus-What-Are-They-Up-To-300x229.png";
+            string logo = "ms-appdata:///local/menu.jpg";
+            int conversationId = 384928;
+
+            // Construct the visuals of the toast
+            ToastVisual visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = title
+                        },
+
+                        new AdaptiveText()
+                        {
+                            Text = content
+                        },
+
+                        new AdaptiveImage()
+                        {
+                            Source = image
+                        }
+                    },
+
+                    AppLogoOverride = new ToastGenericAppLogo()
+                    {
+                        Source = logo,
+                        HintCrop = ToastGenericAppLogoCrop.Circle
+                    }
+                }
+            };
+
+            // Construct the actions for the toast (inputs and buttons)
+            ToastActionsCustom actions = new ToastActionsCustom()
+            {
+                Inputs =
+                {
+                    new ToastTextBox("tbReply")
+                    {
+                        PlaceholderContent = "Type a response"
+                    }
+                },
+
+                Buttons =
+                {
+                    new ToastButton("Reply", new QueryString()
+                    {
+                        { "action", "reply" },
+                        { "conversationId", conversationId.ToString() }
+
+                    }.ToString())
+                    {
+                        ActivationType = ToastActivationType.Background,
+                        ImageUri = "Assets/Reply.png",
+
+                        // Reference the text box's ID in order to
+                        // place this button next to the text box
+                        TextBoxId = "tbReply"
+                    },
+
+                    new ToastButton("Like", new QueryString()
+                    {
+                        { "action", "like" },
+                        { "conversationId", conversationId.ToString() }
+
+                    }.ToString())
+                    {
+                        ActivationType = ToastActivationType.Background
+                    },
+
+                    new ToastButton("View", new QueryString()
+                    {
+                        { "action", "viewImage" },
+                        { "imageUrl", image }
+
+                    }.ToString())
+                }
+            };
+
+
+            // Now we can construct the final toast content
+            ToastContent toastContent = new ToastContent()
+            {
+                Visual = visual,
+                Actions = actions,
+
+                // Arguments when the user taps body of toast
+                Launch = new QueryString()
+                {
+                    { "action", "viewConversation" },
+                    { "conversationId", conversationId.ToString() }
+
+                }.ToString()
+            };
+
+            // And create the toast notification
+            ToastNotification notification = new ToastNotification(toastContent.GetXml());
+
+            // And then send the toast
+            ToastNotificationManager.CreateToastNotifier().Show(notification);
+        }
+        private void btnShowToastSched_Click(object sender, RoutedEventArgs e)
+        {
+            ShowToastNotificationScheduled(3);
+        }
+
+        private void btnShowToast_Click(object sender, RoutedEventArgs e)
+        {
+            ShowToastNotification();
         }
     }
 }
